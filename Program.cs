ee796e851c15eff67ca97fd8c96876a1d2ba0d33
Lera <badasyan.lera@mail.ru>
2022-09-07 23:07:46 +0300

    added decision for task 47,50

--- Program.cs
+++ Program.cs
@@ -1,4 +1,84 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 // Console.WriteLine("Hello, World!");
-
+Console.Clear();
 // Задача 47.Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// Console.WriteLine("Input number of array's lines: ");
+// int linesnum = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input number of array's columns: ");
+// int columnsnum = Convert.ToInt32(Console.ReadLine());
+// double[,] numbers = new double[linesnum, columnsnum];
+// ArrayRndm(numbers);
+// PrintArray(numbers);
+
+// void ArrayRndm(double[,] array)
+// {
+//     Random rndm =new Random();
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+//         }
+//     }
+// }
+
+// void PrintArray(double[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + "   ");
+//         }
+//         Console.WriteLine("");
+//     }
+// }
+
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+
+// Console.WriteLine("Input number of array's lines: ");
+//  int linesnum = Convert.ToInt32(Console.ReadLine());
+//  Console.WriteLine("Input number of array's columns: ");
+//  int columnsnum = Convert.ToInt32(Console.ReadLine());
+//  int[,] numbers = new int[linesnum, columnsnum];
+//  ArrayRndm(numbers);
+//  PrintArray(numbers);
+ 
+//  void ArrayRndm(int[,] array)
+//  {
+//      Random rndm =new Random();
+//      for (int i = 0; i < array.GetLength(0); i++)
+//      {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//          {
+//              array[i, j] = Convert.ToInt32(new Random().Next(10,50));
+//          }
+//      }
+//  }
+
+//  void PrintArray(int[,] array)
+//  {
+//      for (int i = 0; i < array.GetLength(0); i++)
+//      {
+//          for (int j = 0; j < array.GetLength(1); j++)
+//          {
+//              Console.Write(array[i, j] + "   ");
+//          }
+//          Console.WriteLine("");
+//      }
+// }
+// Console.WriteLine("Input any number: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// {
+//     if (n>50 || n< 10)
+// {
+//     Console.Write("Nonexistent number of array");
+// }
+//     else
+//     {
+//     Console.Write("Existent number of array");
+//     }
+// }

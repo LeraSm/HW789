fef74f7a0773591ca8a6442bf1805414c1bf65f9
Lera <badasyan.lera@mail.ru>
2022-09-10 20:24:48 +0300

    added decision for task 52

--- Program.cs
+++ Program.cs
@@ -82,3 +82,48 @@ Console.Clear();
 //     Console.Write("Existent number of array");
 //     }
 // }
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Console.WriteLine("Input number of array's lines: ");
+// int linesnum = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input number of array's columns: ");
+// int columnsnum = Convert.ToInt32(Console.ReadLine());
+// int[,] numbers = new int[linesnum, columnsnum];
+// ArrayRndm(numbers);
+// PrintArray(numbers);
+
+// for (int j = 0; j < numbers.GetLength(1); j++)
+// {
+//     double avarage = 0;
+//     for (int i = 0; i < numbers.GetLength(0); i++)
+//     {
+//         avarage = (avarage + numbers[i, j]);
+//     }
+//     avarage = avarage / linesnum;
+//     Console.Write($"{avarage:f1}");
+//     Console.Write(";");
+// }
+ 
+// void ArrayRndm(int[,] array)
+// {
+// Random rndm =new Random();
+//  for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//      for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         array[i, j] = Convert.ToInt32(new Random().Next(1,30));
+//     }
+//     }
+// }
+
+//   void PrintArray(int[,] array)
+// {
+//   for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//     Console.Write(array[i, j] + "   ");
+//     }
+//     Console.WriteLine("");
+//     }
+// }
